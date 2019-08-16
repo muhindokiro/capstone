1ef04c5873ef4ef6a960c60ed9af838b8506a289
muhindokiro <blaize1143@gmail.com>
2019-08-16 12:02:59 +0300

    updated urls.py file add profile path

--- doctorwho/urls.py
+++ doctorwho/urls.py
@@ -7,11 +7,9 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^profile$', views.profile, name='profile'),
-    # url(r'^view$', views.view, name='view'),
     url(r'^new/complication$', views.new_complication, name='new-complication'),
     url(r'^new/feedback$', views.feedback, name='feedback'),
     url(r'^aboutus$', views.aboutus, name='aboutus')
-    # url(r'^update/profile$', views.update_profile, name='update-profile')
 ]
 
 if settings.DEBUG:
